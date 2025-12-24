946143b1c8711cef9df128c41d2161cacf21db4c
Domer078 <khomotsomabala05@gmail.com>
2025-12-24 13:11:14 +0200

    Added moment.js for date and time handling

---
+++ Javascript/index.js
@@ -0,0 +1,23 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#Los-Angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+  let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDate.innerHTML = losAngelesCurrentTime.format("MMMM DO YYYY");
+  losAngelesTime.innerHTML = losAngelesCurrentTime.format(
+    "h:mm:ss [<strong>A</strong>]"
+  );
+
+  let capeTownElement = document.querySelector("#Cape-Town");
+  let capeTownDate = capeTownElement.querySelector(".date");
+  let capeTownTime = capeTownElement.querySelector(".time");
+  let capeTownCurrentTime = moment().tz("Africa/Cape Town");
+
+  capeTownDate.innerHTML = capeTownCurrentTime.format("MMMM DO YYYY");
+  capeTownTime.innerHTML = capeTownCurrentTime.format(
+    "h:mm:ss [<strong>A</strong>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
